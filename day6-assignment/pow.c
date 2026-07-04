cdea69e0005b051a8f219d771c1d9188d33bbb62
mahubgit123 <25110cn603@glbitm.ac.in>
2026-07-04 20:35:16 +0530

    day6

---
+++ day6-assignment/pow.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, n, i, result = 1;
+
+    printf("Enter the base (x): ");
+    scanf("%d", &x);
+
+    printf("Enter the power (n): ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        result = result * x;
+    }
+
+    printf("%d raised to the power %d = %d", x, n, result);
+
+    return 0;
+}
